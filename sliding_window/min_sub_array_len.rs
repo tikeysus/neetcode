e6ae440a0ed2fd8511e303c79ab6b2425f7cc846
Tigran Tumanyan <tigrantumanyann@gmail.com>
2025-06-04 15:29:42 -0400

    Simpler than it seemed at first.

--- sliding_window/min_sub_array_len.rs
+++ sliding_window/min_sub_array_len.rs
@@ -1,45 +1,31 @@
 //Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
 
-use std::convert::TryInto;
-
 fn main(){
-	let target = 7; 
-	let nums = vec![2,3,1,2,4,3];
+	let target = 4; 
+	let nums = vec![1,4,4];
 	println!("{}", min_sub_array_len(target, nums));
-	//min_sub_array_len(target, nums); 
 }
 
 fn min_sub_array_len(target: i32, nums: Vec<i32>) -> i32{ 
-	let (mut right, mut left, mut sum) = (0, 0, 0); 
-	let mut min = 0; 
+	let (mut left, mut right, mut sum, mut min) = (0, 0, 0, 0); 
 
-	while right < (nums.len() - 1){
-		if sum - nums[left] >= target{
+	while right < nums.len(){
+		sum += nums[right]; 
+		right += 1; 
+
+		while sum - nums[left] >= target{
 			sum -= nums[left]; 
 			left += 1; 
-			
-			if min == 0{
-				min = right - left; 
-			}
-			
-			else if right - left < min{
-				min = right - left; 
-			}
-			
 		}
-		
-		else{
-			right += 1; 
-			sum += nums[right]; 
 
-			if min == 0{
-				min = right - left; 
-			}
+		if min == 0 && sum >= target{
+			min = right - left; 
+		}
 
-			else if right - left < min{
-				min = right - left; 
-			}
+		else if right - left < min && sum >= target{
+			min = right - left; 
 		}
 	}
-	return min.try_into().unwrap();	
+
+	return min as i32; 
 }
